bf55f5270255475c7cc7e6f452b76a8a6ceddf20
FlamingGear <flaminggear@hotmail.co.uk>
2014-03-24 13:57:46 +0000

    Scene Setup
    All Scenes have been created bar the main game scene, Main menu is
    essentially complete, other menus all need doing.

---
+++ Assets/Scenes/FishStocks/FishStockMenu.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FishStockMenu : MonoBehaviour 
+{
+    void OnGUI()
+    {
+        const int buttonWidth = 256;
+        const int buttonHeight = 32;
+
+        //Draw StartGame Button
+        if (GUI.Button(
+                new Rect(   Screen.width / 4 - (buttonWidth / 2),
+                            6 * (Screen.height / 9) - (buttonHeight / 2),
+                            buttonWidth, buttonHeight), "Play Game"))
+        {
+            //Button Do Method
+            Application.LoadLevel("CompanySummary");
+        }
+
+        //Draw HighScores Button
+        if (GUI.Button(
+                new Rect(   Screen.width / 4 - (buttonWidth / 2),
+                            8 * (Screen.height / 9) - (buttonHeight / 2),
+                            buttonWidth, buttonHeight), "High Scores"))
+        {
+            //Button Do Method
+            Application.LoadLevel("HighScores");
+        }
+
+        //Draw Options Button
+        if (GUI.Button(
+                new Rect(   3 * (Screen.width / 4) - (buttonWidth / 2),
+                            6 * (Screen.height / 9) - (buttonHeight / 2),
+                            buttonWidth, buttonHeight), "Options"))
+        {
+            //Button Do Method
+            Application.LoadLevel("Options");
+        }
+
+        //Draw Exit Button
+        if (GUI.Button(
+                new Rect(   3 * (Screen.width / 4) - (buttonWidth / 2),
+                            8 * (Screen.height / 9) - (buttonHeight / 2),
+                            buttonWidth, buttonHeight), "Exit Game"))
+        {
+            //Button Do Method
+            Application.Quit();
+        }
+    }
+}
